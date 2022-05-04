90ff605afc7680f0723165c35e9bc19bfbd19bfa
ozankayaa <oznkaya15@gmail.com>
2022-05-04 10:10:53 +0300

    sm8250-common: Drop Atrace Hal compilation

--- kona.mk
+++ kona.mk
@@ -80,9 +80,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 endif
 
-# Atrace
-PRODUCT_PACKAGES += \
-    android.hardware.atrace@1.0-service
 
 # ART
 # Optimize everything for preopt
