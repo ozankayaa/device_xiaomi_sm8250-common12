0eb5b013c07c4c9905ccbfacfb3cb4ccd86f8f17
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-28 10:27:37 +0200

    sm8250-common: Drop atrace HIDL
    Change-Id: Iaaaf62e9dda26dd15d51f5975d9f21fe1037d2eb

--- kona.mk
+++ kona.mk
@@ -75,10 +75,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 endif
 
-# Atrace
-PRODUCT_PACKAGES += \
-    android.hardware.atrace@1.0-service
-
 
 # ANT+
 PRODUCT_PACKAGES += \
@@ -88,6 +84,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
+
 
 # Audio
 PRODUCT_PACKAGES += \
