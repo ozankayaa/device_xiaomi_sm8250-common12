7102d62f1731c7fd6ae33792f7ca08a7f220af44
goodmeow <aarunalr@nusantararom.org>
2022-08-14 09:41:08 +0530

    sm8250-common: add back touchfeature * miss merged lmao

--- kona.mk
+++ kona.mk
@@ -480,6 +480,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.xiaomi.hardware.touchfeature@1.0.vendor
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti
